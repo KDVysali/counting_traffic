cd4c4261040bf7ae915ca9d5f722126126de0d82
KDVysali <143497993+KDVysali@users.noreply.github.com>
2025-06-19 09:47:46 +0530

    app.py

--- app.py
+++ app.py
@@ -55,4 +55,5 @@ def process_video():
     return jsonify(dict(class_counts))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
+    app.run(host='0.0.0.0', port=port)
